51d546f4a1f400f2cde9f07efa1f996f9fb95ea4
Mateus <matbarbieri8@gmail.com>
2023-12-22 16:41:11 -0300

    missed file

---
+++ src/domain/groups/miniature.py
@@ -0,0 +1,24 @@
+from domain.models import Miniature
+from domain.usecases.get_database_controller import GetDatabaseController
+from infrastructure.core_dependency_injection_factories.core_dependency_injection_factory_postgres import CoreDependencyInjectionFactoryJson
+from src.infrastructure.core_bridge_adapters import CoreBridgeAdapter
+from domain.usecases import GetInjection
+
+def create_miniature_group()->Miniature:
+    core_dependency_injection_factory = CoreDependencyInjectionFactoryJson()
+    core_bridge = CoreBridgeAdapter()
+
+    get_injection = GetInjection(
+        core_bridge, core_dependency_injection_factory
+    )
+
+    injection = get_injection.call()
+    database_controller = GetDatabaseController(
+        injection.database_connection_factory
+    )
+
+    database_controller.call()
+
+    
+
+
